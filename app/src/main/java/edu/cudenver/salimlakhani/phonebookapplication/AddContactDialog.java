2602179f8ee697ece09172d490c85e3031e9d1f4
lakhanis <salim.lakhani@ucdenver.edu>
2021-11-03 23:14:28 -0600

    Updated with menu toolbars

--- app/src/main/java/edu/cudenver/salimlakhani/phonebookapplication/AddContactDialog.java
+++ app/src/main/java/edu/cudenver/salimlakhani/phonebookapplication/AddContactDialog.java
@@ -4,11 +4,14 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import java.lang.ref.SoftReference;
 
 import edu.cudenver.salimlakhani.phonebookapplication.databinding.DialogAddContactBinding;
 
@@ -23,11 +26,77 @@ public class AddContactDialog extends DialogFragment {
         binding = DialogAddContactBinding.inflate(LayoutInflater.from(getContext()));
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setView(binding.getRoot());
+        binding.addContact.inflateMenu(R.menu.menu_add_contact);
 
         binding.buttonMainMenu.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 dismiss ();
             }
+        });
+
+        binding.addContact.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.action_exit:
+                    dismiss();
+                    return true;
+                case R.id.action_clear:
+                    return true;
+                case R.id.action_save:
+                    return true;
+
+            }
+            return true;
+
+        });
+
+        binding.buttonClear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                binding.textInputName.setText ("");
+                binding.textInputPhone.setText ("");
+                binding.textInputEmail.setText("");
+                binding.textInputStreet.setText("");
+                binding.textInputCity.setText("");
+                binding.textInputState.setText ("");
+                binding.textInputZip.setText("");
+
+                binding.radioButtonBusiness.setChecked(true);
+
+                binding.textInputName.requestFocus();
+            }
+        });
+
+        binding.buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = binding.textInputName.getText().toString();
+                String phone = binding.textInputPhone.getText().toString();
+                String email = binding.textInputEmail.getText().toString();
+                String street = binding.textInputStreet.getText().toString();
+                String city = binding.textInputCity.getText().toString();
+                String state = binding.textInputState.getText().toString();
+                String zip = binding.textInputZip.getText().toString();
+
+                String contactType = "";
+
+                if (binding.radioButtonBusiness.isChecked()) {
+                    contactType = "Business";
+                }
+                else if (binding.radioButtonFamily.isChecked()) {
+                    contactType = "Family";
+                }
+                else {
+                    contactType = "Friend";
+                }
+
+                Contact contact = new Contact (name, phone, email, street, city, state, zip, contactType);
+
+                MainActivity mainActivity = (MainActivity) getActivity();
+                mainActivity.addContact(contact);
+                dismiss();
+
+            }
+
         });
 
 
--- app/src/main/java/edu/cudenver/salimlakhani/phonebookapplication/MainActivity.java
+++ app/src/main/java/edu/cudenver/salimlakhani/phonebookapplication/MainActivity.java
@@ -1,36 +1,55 @@
 package edu.cudenver.salimlakhani.phonebookapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import edu.cudenver.salimlakhani.phonebookapplication.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private ArrayList<Contact> list;
+    private ContactAdapter contactAdapter;
+    private RecyclerView recyclerView;
+    private String type;
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        prefs = getSharedPreferences("phonebook", Context.MODE_PRIVATE);
+        editor = prefs.edit();
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+
+        list = new ArrayList<Contact>();
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +58,16 @@ public class MainActivity extends AppCompatActivity {
                 addContactDialog.show (getSupportFragmentManager(), "");
             }
         });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        contactAdapter = new ContactAdapter(this, list );
+        type= prefs.getString("type", "name");
+        contactAdapter.setType(type);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerView.setAdapter(contactAdapter);
     }
 
     @Override
@@ -56,11 +85,45 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add) {
+            AddContactDialog addContactDialog = new AddContactDialog();
+            addContactDialog.show (getSupportFragmentManager(), "");
+            return true;
+        }
+        else if (id == R.id.action_name) {
+            type = "name";
+            editor.putString("type", type);
+            editor.commit();
+            contactAdapter.setType(type);
+            contactAdapter.notifyDataSetChanged();
+            return true;
+        }
+        else if (id == R.id.action_phone) {
+            type = "phone";
+            contactAdapter.setType(type);
+            editor.putString("type", type);
+            editor.commit();
+            contactAdapter.notifyDataSetChanged();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void addContact (Contact contact) {
+        list.add(contact);
+        Log.i ("info", "Number of Contact is " + list.size());
+        contactAdapter.notifyDataSetChanged();
+    }
+
+
+    public void showContact (int contactToShow) {
+        Log.i ("info", "Inside showContact method");
+        ViewContactDialog viewContactDialog = new ViewContactDialog();
+        viewContactDialog.setContact(list.get(contactToShow));
+        viewContactDialog.show(getSupportFragmentManager(), "");
+
     }
 
 }
